4450ca8b0c02c39b4be949da844825c68e034136
Mario Juretić <mario@weblio.eu>
2023-01-04 21:48:33 +0100

    split expenses into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
+
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => (
   <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+    <ExpenseDate date={props.date} />
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
