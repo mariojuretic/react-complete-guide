72e92e3131701906b0057fc7d012208d37f08537
Mario Juretić <mario@weblio.eu>
2023-01-04 16:34:56 +0100

    adding more complex jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,13 @@
-const ExpenseItem = () => <h2>Expense item!</h2>;
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
