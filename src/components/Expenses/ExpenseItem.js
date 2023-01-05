fa73e5a6cc80feb5a8be09913f3f983c19bd1b6e
Mario Juretić <mario@weblio.eu>
2023-01-05 14:39:17 +0100

    adding our first event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,21 @@ import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => (
-  <Card className="expense-item">
-    <ExpenseDate date={props.date} />
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
-  </Card>
-);
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
