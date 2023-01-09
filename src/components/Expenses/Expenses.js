73dd53543c5fc42864218bea9b77e250a033a65f
Mario Juretić <mario@weblio.eu>
2023-01-09 16:20:36 +0100

    added dynamic expense rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,21 +20,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        <ExpenseItem
-          date={props.items[0].date}
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          date={props.items[1].date}
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-        />
-        <ExpenseItem
-          date={props.items[2].date}
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            amount={expense.amount}
+          />
+        ))}
       </Card>
     </div>
   );
