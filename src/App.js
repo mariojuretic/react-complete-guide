f8999651df93cbec1472a7ed5d3b8c63000fc619
Mario Juretić <mario@weblio.eu>
2023-01-12 00:22:42 +0100

    added AddUser component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
-const App = () => <div />;
+import AddUser from "./components/Users/AddUser";
+
+const App = () => {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+};
 
 export default App;
