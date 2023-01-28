2e8a70b2dd5b05b3b4e81c438e4335081e265c74
Mario Juretić <mario@weblio.eu>
2023-01-28 15:01:14 +0100

    added default values into context for autocompletion

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const AuthContext = createContext({ isLoggedIn: false });
+const AuthContext = createContext({ isLoggedIn: false, onLogout: () => {} });
 
 export default AuthContext;
