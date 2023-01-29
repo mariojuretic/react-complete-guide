39d90f6693feddfcc9406f214acbe2dba870c6c4
Mario Juretić <mario@weblio.eu>
2023-01-29 19:16:47 +0100

    added Header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpeg";
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
