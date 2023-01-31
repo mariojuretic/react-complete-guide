620dd8e72797b64a527a98383695dbd8c930c24b
Mario Juretić <mario@weblio.eu>
2023-01-31 07:53:00 +0100

    making cart items removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,9 +9,6 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
-
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
@@ -30,6 +27,34 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = state.items.concat(action.item);
     }
+
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
 
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
