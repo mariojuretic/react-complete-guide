188c60424907a9c9e88a373fa5f73b72f7ae1343
Mario Juretić <mario@weblio.hr>
2023-02-08 10:40:19 +0100

    added a conditional paragraph

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
+import Button from "./components/UI/Button/Button";
 import "./App.css";
 
 const App = () => {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP COMPONENT RUNNING");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 };
