3ff6c0b662984a583ff5eb57dc2bc1b5ac6dc2d1
Mario Juretić <mario@weblio.hr>
2023-02-16 17:32:16 +0100

    starting setup

---
+++ src/components/MoviesList.js
@@ -0,0 +1,19 @@
+import Movie from "./Movie";
+import styles from "./MoviesList.module.css";
+
+const MoviesList = (props) => {
+  return (
+    <ul className={styles["movies-list"]}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MoviesList;
