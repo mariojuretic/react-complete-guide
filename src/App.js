572003a011e2beb188e4678e7a01164ab5df5fda
Mario Juretić <mario@weblio.hr>
2023-02-17 15:36:50 +0100

    starting setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ForwardCounter from "./components/ForwardCounter";
+import BackwardCounter from "./components/BackwardCounter";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ForwardCounter />
+      <BackwardCounter />
+    </>
   );
-}
+};
 
 export default App;
