e3eceebf35746c61f69fdd93274ca79b74a4a9e9
Mario Juretić <mario@weblio.hr>
2023-02-22 10:18:09 +0100

    starting setup

---
+++ src/components/BasicForm.js
@@ -0,0 +1,25 @@
+const BasicForm = () => {
+  return (
+    <form>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+        </div>
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail Address</label>
+        <input type="email" id="email" />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicForm;
