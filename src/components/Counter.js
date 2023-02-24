21654a9d51f588d9a2547547683f8d2de52b1c57
Mario Juretić <mario@weblio.hr>
2023-02-24 10:08:38 +0100

    accessing redux store with useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
+
 import styles from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
-      <div className={styles.value}>-- COUNTER VALUE --</div>
+      <div className={styles.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
