b1d2bd4fba9d2bae19d214e8f715bd8a235e8479
Mario Juretić <mario@weblio.hr>
2023-02-27 19:19:31 +0100

    splitting redux slices into separate files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import { authActions } from "../store";
+import { authActions } from "../store/auth";
 import styles from "./Header.module.css";
 
 const Header = () => {
