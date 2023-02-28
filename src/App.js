4944ac418d7af23da8c65970b0d16d648820d920
Mario Juretić <mario@weblio.hr>
2023-02-28 15:47:43 +0100

    created a router and first two routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
