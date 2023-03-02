9a12902327269410936be2a79e3ab140185f97b1
Mario Juretić <mario@weblio.hr>
2023-03-02 12:08:28 +0100

    fetching events from a backend api

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,21 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Some event" },
-  { id: "e2", title: "Another event" },
-];
+import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setIsLoading(true);
+
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const responseData = await response.json();
+        setFetchedEvents(responseData.events);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 };
