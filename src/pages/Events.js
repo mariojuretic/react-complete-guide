9dfcd7d48517478e306f63c1e3ddf9acf58d88d2
Mario Juretić <mario@weblio.hr>
2023-03-02 12:16:49 +0100

    using loader data in Events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,40 +1,11 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const events = useLoaderData();
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      setIsLoading(true);
-
-      const response = await fetch("http://localhost:8080/events");
-
-      if (!response.ok) {
-        setError("Fetching events failed.");
-      } else {
-        const responseData = await response.json();
-        setFetchedEvents(responseData.events);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchEvents();
-  }, []);
-
-  return (
-    <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    </>
-  );
+  return <EventsList events={events} />;
 };
 
 export default EventsPage;
