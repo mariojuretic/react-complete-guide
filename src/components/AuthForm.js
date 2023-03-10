913ef8e9dadec66fe505853bbeac49ca5e68d638
Mario Juretić <mario@weblio.hr>
2023-03-10 23:09:57 +0100

    outputting validation errors

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,15 +1,33 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 import styles from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const data = useActionData();
+  const navigation = useNavigation();
+
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={styles.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required />
@@ -22,7 +40,9 @@ const AuthForm = () => {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Log in"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
