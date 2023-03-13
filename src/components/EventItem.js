cb093020cc79696a4a6712c1f5d97d6de0e9aef4
Mario Juretić <mario@weblio.hr>
2023-03-13 15:12:53 +0100

    updating ui based on auth status

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,8 +1,9 @@
-import { Link, useSubmit } from "react-router-dom";
+import { Link, useSubmit, useRouteLoaderData } from "react-router-dom";
 
 import styles from "./EventItem.module.css";
 
 const EventItem = ({ event }) => {
+  const token = useRouteLoaderData("root");
   const submit = useSubmit();
 
   const startDeleteHandler = () => {
@@ -19,10 +20,12 @@ const EventItem = ({ event }) => {
       <h1>{event.title}</h1>
       <time>{event.date}</time>
       <p>{event.description}</p>
-      <menu className={styles.actions}>
-        <Link to="edit">Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
-      </menu>
+      {token && (
+        <menu className={styles.actions}>
+          <Link to="edit">Edit</Link>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </menu>
+      )}
     </article>
   );
 };
